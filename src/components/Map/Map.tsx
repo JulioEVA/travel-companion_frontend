f147f4d8b6905d0930ee2b68575d3a8bc8fdb649
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-03-01 14:11:34 -0700

    Add initial components and style files

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import { Rating } from '@material-ui/lab';
+
+import useStyles from './MapStyles';
+
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(max-width:600px)');
+
+  const coordinates = { lat: 0, lng: 0 };
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyC7ugksykKBwXE47s6ef3l8zczwSVI8mGM' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
