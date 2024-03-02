77282e4742598b98be5771a09128453c59af95a9
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-03-01 17:03:37 -0700

    Add styles and info display

--- src/components/PlaceDetails/PlaceDetails.tsx
+++ src/components/PlaceDetails/PlaceDetails.tsx
@@ -1,7 +1,101 @@
 import React from 'react';
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Chip,
+} from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
 
-const PlaceDetails = () => {
-  return <h1>PlaceDetails</h1>;
+import useStyles from './PlaceDetailsStyles';
+import { Place } from '../List/List';
+
+const PlaceDetails: React.FC<{ place: Place }> = ({ place }) => {
+  const classes = useStyles();
+
+  return (
+    <Card elevation={6}>
+      <CardMedia
+        style={{ height: 350 }}
+        image={
+          place.photo
+            ? place.photo.images.large.url
+            : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+        }
+        title={place.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant='h5'>
+          {place.name}
+        </Typography>
+        <Box display='flex' justifyContent='space-between'>
+          <Typography variant='subtitle1'>Price</Typography>
+          <Typography gutterBottom variant='subtitle1'>
+            {place.price_level}
+          </Typography>
+        </Box>
+        <Box display='flex' justifyContent='space-between'>
+          <Typography variant='subtitle1'>Ranking</Typography>
+          <Typography gutterBottom variant='subtitle1'>
+            {place.ranking}
+          </Typography>
+        </Box>
+        {place?.awards?.map((award) => (
+          <Box my={1} display='flex' justifyContent='space-between'>
+            <img src={award.images.small.url} alt={award.display_name} />
+            <Typography variant='subtitle2' color='textSecondary'>
+              {award.year}
+            </Typography>
+          </Box>
+        ))}
+        {place?.cuisine?.map(({ name }: { name: string }) => (
+          <Chip key={name} size='small' label={name} className={classes.chip} />
+        ))}
+        {place?.address && (
+          <Typography
+            gutterBottom
+            variant='subtitle2'
+            color='textSecondary'
+            className={classes.subtitle}
+          >
+            <LocationOnIcon /> {place.address}
+          </Typography>
+        )}
+        {place?.phone && (
+          <Typography
+            gutterBottom
+            variant='subtitle2'
+            color='textSecondary'
+            className={classes.subtitle}
+          >
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
+        <CardActions>
+          <Button
+            size='small'
+            color='primary'
+            onClick={() => window.open(place.web_url, '__blank')}
+          >
+            Trip Advisor
+          </Button>
+          <Button
+            size='small'
+            color='primary'
+            onClick={() => window.open(place.website, '__blank')}
+          >
+            Website
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default PlaceDetails;
