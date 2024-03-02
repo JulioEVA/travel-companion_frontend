6b227444d0e7816d870d893880213846583c0e71
JulioEVA <julio-emmanuel2011@hotmail.com>
2024-03-01 23:03:24 -0700

    Extract sensitive keys into env variables

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,29 +1,90 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact, { Coords } from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import { Rating } from '@material-ui/lab';
+import Rating from '@material-ui/lab/Rating';
 
+import mapStyles from '../../vendor/mapStyles';
 import useStyles from './MapStyles';
+import { Coordinates, Bounds as ApiBounds } from '../App/App';
+import { Place } from '../List/List';
+import Marker from '../Marker/Marker';
 
-const Map = () => {
+type MapProps = {
+  setCoordinates: (coordinates: Coordinates) => void;
+  setBounds: (bounds: ApiBounds) => void;
+  setChildClicked: (child: string) => void;
+  coordinates: Coordinates;
+  places: Place[];
+};
+
+const Map: React.FC<MapProps> = ({
+  setCoordinates,
+  setBounds,
+  setChildClicked,
+  coordinates,
+  places,
+}) => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)');
-
-  const coordinates = { lat: 0, lng: 0 };
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyC7ugksykKBwXE47s6ef3l8zczwSVI8mGM' }}
-        defaultCenter={coordinates}
+        bootstrapURLKeys={{
+          key: import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string,
+        }}
         center={coordinates}
+        defaultCenter={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={''}
-        onChange={''}
-        onChildClick={''}
-      ></GoogleMapReact>
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles,
+        }}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({
+            ne: e.marginBounds.ne as Coords,
+            sw: e.marginBounds.sw as Coords,
+          });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
+      >
+        {places?.map((place, i) => (
+          <Marker
+            className={classes.markerContainer}
+            lat={place.latitude}
+            lng={place.longitude}
+            key={i}
+          >
+            {isMobile ? (
+              <LocationOnOutlinedIcon color='primary' fontSize='large' />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant='subtitle2'
+                  gutterBottom
+                >
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+                  }
+                  alt={place.name}
+                />
+                <Rating size='small' value={Number(place.rating)} readOnly />
+              </Paper>
+            )}
+          </Marker>
+        ))}
+      </GoogleMapReact>
     </div>
   );
 };
